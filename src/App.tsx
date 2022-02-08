851f7a8d68a8a2f67fbdae07ed08d9d8eb514a21
tochka-js <tochka.byl@gmail.com>
2022-02-09 01:28:07 +0300

    my first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://cdn.wallpapersafari.com/22/61/NWEx1Y.jpg" alt=" "/>
+            </header>
+            <nav className='nav'>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+        </div>
+    )
+
 }
 
 export default App;
---
+++ src/components/Messages.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export const Messages = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
